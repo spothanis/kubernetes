spothanis/kubernetes

commit f1fed3bcfcff57e522d0ca90c80d95c886ce5119

Remove support for building "run containers".
This was untested, broken and largely unused.

Related to #12 and #2455#issuecomment-68803245.

build/common.sh
@@ -34,7 +34,6 @@ source hack/lib/init.sh
 # Incoming options
 #
 readonly KUBE_SKIP_CONFIRMATIONS="${KUBE_SKIP_CONFIRMATIONS:-n}"
-readonly KUBE_BUILD_RUN_IMAGES="${KUBE_BUILD_RUN_IMAGES:-n}"
 readonly KUBE_GCS_UPLOAD_RELEASE="${KUBE_GCS_UPLOAD_RELEASE:-n}"
 readonly KUBE_GCS_NO_CACHING="${KUBE_GCS_NO_CACHING:-y}"
 readonly KUBE_GCS_MAKE_PUBLIC="${KUBE_GCS_MAKE_PUBLIC:-y}"
@@ -85,18 +84,6 @@ readonly DOCKER_DATA_MOUNT_ARGS=(
   --volume "${REMOTE_OUTPUT_GOPATH}"
   --volume "${REMOTE_GODEP_GOPATH}"
 )
-
-
-readonly KUBE_RUN_IMAGE_BASE="kubernetes"
-readonly KUBE_RUN_IMAGES=(
-  kube-apiserver
-  kube-controller-manager
-  kube-proxy
-  kube-scheduler
-  kubelet
-  bootstrap
-)
-
 
 # This is where the final release artifacts are created locally
 readonly RELEASE_STAGE="${LOCAL_OUTPUT_ROOT}/release-stage"
@@ -317,30 +304,6 @@ function kube::build::build_image_cross() {
   kube::build::docker_build "${KUBE_BUILD_IMAGE_CROSS}" "${build_context_dir}"
 }
 
-# Builds the runtime image.  Assumes that the appropriate binaries are already
-# built and in $LOCAL_OUTPUT_BINPATH.
-function kube::build::run_image() {
-  [[ ${KUBE_BUILD_RUN_IMAGES} =~ ^[yY]$ ]] || return 0
-
-  local -r build_context_base="${LOCAL_OUTPUT_IMAGE_STAGING}/${KUBE_RUN_IMAGE_BASE}"
-
-  # First build the base image.  This one brings in all of the binaries.
-  mkdir -p "${build_context_base}"
-  tar czf "${build_context_base}/kube-bins.tar.gz" \
-    -C "${LOCAL_OUTPUT_BINPATH}/linux/amd64" \
-    "${KUBE_RUN_IMAGES[@]}"
-  cp -R build/run-images/base/* "${build_context_base}/"
-  kube::build::docker_build "${KUBE_RUN_IMAGE_BASE}" "${build_context_base}"
-
-  local b
-  for b in "${KUBE_RUN_IMAGES[@]}" ; do
-    local sub_context_dir="${build_context_base}-$b"
-    mkdir -p "${sub_context_dir}"
-    cp -R build/run-images/$b/* "${sub_context_dir}/"
-    kube::build::docker_build "${KUBE_RUN_IMAGE_BASE}-$b" "${sub_context_dir}"
-  done
-}
-
 # Build a docker image from a Dockerfile.
 # $1 is the name of the image to build
 # $2 is the location of the "context" directory, with the Dockerfile at the root.
@@ -375,13 +338,6 @@ function kube::build::clean_image() {
 
 function kube::build::clean_images() {
   kube::build::clean_image "${KUBE_BUILD_IMAGE}"
-
-  kube::build::clean_image "${KUBE_RUN_IMAGE_BASE}"
-
-  local b
-  for b in "${KUBE_RUN_IMAGES[@]}" ; do
-    kube::build::clean_image "${KUBE_RUN_IMAGE_BASE}-${b}"
-  done
 
   echo "+++ Cleaning all other untagged docker images"
   "${DOCKER[@]}" rmi $("${DOCKER[@]}" images -q --filter 'dangling=true') 2> /dev/null || true
@@ -732,59 +688,6 @@ function kube::release::gcs::ensure_release_bucket() {
     echo "Creating Google Cloud Storage bucket: $KUBE_GCS_RELEASE_BUCKET"
     gsutil mb -p "${GCLOUD_PROJECT}" "gs://${KUBE_GCS_RELEASE_BUCKET}"
   fi
-}
-
-function kube::release::gcs::ensure_docker_registry() {
-  local -r reg_container_name="gcs-registry"
-
-  local -r running=$("${DOCKER[@]}" inspect ${reg_container_name} 2>/dev/null \
-    | build/json-extractor.py 0.State.Running 2>/dev/null)
-
-  [[ "$running" != "true" ]] || return 0
-
-  # Grovel around and find the OAuth token in the gcloud config
-  local -r boto=~/.config/gcloud/legacy_credentials/${GCLOUD_ACCOUNT}/.boto
-  local refresh_token
-  refresh_token=$(grep 'gs_oauth2_refresh_token =' "$boto" | awk '{ print $3 }')
-
-  if [[ -z "$refresh_token" ]]; then
-    echo "Couldn't find OAuth 2 refresh token in ${boto}" >&2
-    return 1
-  fi
-
-  # If we have an old one sitting around, remove it
-  "${DOCKER[@]}" rm ${reg_container_name} >/dev/null 2>&1 || true
-
-  echo "+++ Starting GCS backed Docker registry"
-  local -ra docker_cmd=(
-    "${DOCKER[@]}" run -d "--name=${reg_container_name}"
-    -e "GCS_BUCKET=${KUBE_GCS_RELEASE_BUCKET}"
-    -e "STORAGE_PATH=${KUBE_GCS_DOCKER_REG_PREFIX}"
-    -e "GCP_OAUTH2_REFRESH_TOKEN=${refresh_token}"
-    -p 127.0.0.1:5000:5000
-    google/docker-registry
-  )
-
-  "${docker[@]}"
-
-  # Give it time to spin up before we start throwing stuff at it
-  sleep 5
-}
-
-function kube::release::gcs::push_images() {
-  [[ ${KUBE_BUILD_RUN_IMAGES} =~ ^[yY]$ ]] || return 0
-
-  kube::release::gcs::ensure_docker_registry
-
-  # Tag each of our run binaries with the right registry and push
-  local b image_name
-  for b in "${KUBE_RUN_IMAGES[@]}" ; do
-    image_name="${KUBE_RUN_IMAGE_BASE}-${b}"
-    echo "+++ Tagging and pushing ${image_name} to GCS bucket ${KUBE_GCS_RELEASE_BUCKET}"
-    "${DOCKER[@]}" tag "${KUBE_RUN_IMAGE_BASE}-$b" "localhost:5000/${image_name}"
-    "${DOCKER[@]}" push "localhost:5000/${image_name}"
-    "${DOCKER[@]}" rmi "localhost:5000/${image_name}"
-  done
 }
 
 function kube::release::gcs::copy_release_artifacts() {

build/make-run-images.sh
@@ -1,34 +1,0 @@
-#!/bin/bash
-
-# Copyright 2014 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Build the docker image necessary for running Kubernetes
-#
-# This script will make the 'run image' after building all of the necessary
-# binaries.
-
-set -o errexit
-set -o nounset
-set -o pipefail
-
-KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
-KUBE_BUILD_RUN_IMAGES=y
-source "$KUBE_ROOT/build/common.sh"
-
-kube::build::verify_prereqs
-kube::build::build_image
-kube::build::run_build_command build/build-image/make-binaries.sh "$@"
-kube::build::copy_output
-kube::build::run_image

build/release.sh
@@ -37,6 +37,5 @@ if [[ $KUBE_RELEASE_RUN_TESTS =~ ^[yY]$ ]]; then
 fi
 
 kube::build::copy_output
-kube::build::run_image
 kube::release::package_tarballs
 kube::release::gcs::release

build/run-images/apiserver/Dockerfile
@@ -1,25 +1,0 @@
-# Copyright 2014 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file creates a minimal container for running Kubernetes binaries
-
-FROM  kubernetes
-MAINTAINER  Joe Beda <jbeda@google.com>
-
-ENV ETCD_SERVERS http://127.0.0.1:4001
-ENV KUBE_MINIONS ""
-
-ADD . /kubernetes
-
-CMD ["/kubernetes/run.sh"]

build/run-images/apiserver/run.sh
@@ -1,17 +1,0 @@
-#!/bin/bash
-
-# Copyright 2014 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-./kube-apiserver -address=0.0.0.0 -etcd_servers="${ETCD_SERVERS}"

build/run-images/base/Dockerfile
@@ -1,23 +1,0 @@
-# Copyright 2014 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file creates a minimal container for running Kubernetes binaries
-
-FROM google/debian:wheezy
-MAINTAINER  Joe Beda <jbeda@google.com>
-
-WORKDIR /kubernetes
-
-# Upload Kubernetes server binaries
-ADD kube-bins.tar.gz /kubernetes

build/run-images/bootstrap/Dockerfile
@@ -1,22 +1,0 @@
-# Copyright 2014 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file creates a minimal container for running Kubernetes binaries
-
-FROM  kubernetes
-MAINTAINER  Johan Euphrosine <proppy@google.com>
-
-ADD . /kubernetes
-
-CMD ["/kubernetes/run.sh"]

build/run-images/bootstrap/run.sh
@@ -1,56 +1,0 @@
-#!/bin/bash
-
-# Copyright 2014 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# see https://github.com/docker/docker/issues/8395
-HOST_IP=$(ip route show 0.0.0.0/0 | grep -Eo 'via \S+' | awk '{ print $2 }')
-
-KUBELET_IP=$(hostname -i)
-
-cat <<EOF > pod.yaml
-version: v1beta1
-id: cluster-pod
-containers:
-  - name: etcd
-    image: coreos/etcd
-    command: ["-vv"]
-    ports:
-      - name: etcd-port
-        hostPort: 4001
-        containerPort: 4001
-        protocol: TCP
-  - name: kube-apiserver
-    image: kubernetes
-    imagePullPolicy: never
-    ports:
-      - name: kube-apiserver-port
-        hostPort: 8080
-        containerPort: 8080
-        protocol: TCP
-    command: ["/kubernetes/kube-apiserver", "-v=5", "-address=0.0.0.0", "-etcd_servers=http://127.0.0.1:4001"]
-  - name: controller-manager
-    image: kubernetes
-    imagePullPolicy: never
-    command: ["/kubernetes/kube-controller-manager", "-v=5", "-master=127.0.0.1:8080", "-machines=${KUBELET_IP}"]
-  - name: proxy
-    image: kubernetes
-    imagePullPolicy: never
-    command: ["/kubernetes/kube-proxy", "-v=5", "-etcd_servers=http://127.0.0.1:4001"]
-  - name: scheduler
-    image: kubernetes
-    imagePullPolicy: never
-    command: ["/kubernetes/kube-scheduler", "-v=5", "-master=127.0.0.1:8080"]
-EOF
-./kubelet -v=5 -address=0.0.0.0 -hostname_override=${KUBELET_IP} -etcd_servers=http://${HOST_IP}:4001 -config pod.yaml

commit 19b293f540fbda73060f3287329827fe10e34014

Commit f1fed3b broke gcs::release in this function, breaking the build.

build/common.sh
@@ -659,7 +659,6 @@ function kube::release::gcs::release() {
 
   kube::release::gcs::verify_prereqs
   kube::release::gcs::ensure_release_bucket
-  kube::release::gcs::push_images
   kube::release::gcs::copy_release_artifacts
 }
 